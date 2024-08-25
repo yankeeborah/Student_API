268d22dad8e28255c6f297394bc51c3e599d7cc4
Biswa_ASUS <Biswajitborah96@gmail.com>
2024-08-25 23:03:56 +0530

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD_ASPNetCoreWebApi.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string StudentName { get; set; } = null!;
+
+    public string? StudentGender { get; set; }
+
+    public int? Age { get; set; }
+
+    public int? Standard { get; set; }
+
+    public string FatherName { get; set; } = null!;
+}
---
+++ Models/StudentDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_ASPNetCoreWebApi.Models;
+
+public partial class StudentDbContext : DbContext
+{
+    public StudentDbContext()
+    {
+    }
+
+    public StudentDbContext(DbContextOptions<StudentDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC07D4F5AF67");
+
+            entity.Property(e => e.FatherName)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
